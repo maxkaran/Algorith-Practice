03198e2fc552a6a65763edc68a9291d03bb71c9b
Max Karan <maxkaran2@gmail.com>
2018-08-14 15:16:48 -0400

    removed memory leak in PartitionList.c

--- C Code/PartitionList.c
+++ C Code/PartitionList.c
@@ -20,26 +20,28 @@ struct ListNode* partition(struct ListNode* head, int x) {
     ptr = prev -> next; //ptr will point to node before the node being checked
     insertAfter = prev;
     
-    while (ptr != NULL && ptr->val < x)
+    while (ptr != NULL && ptr->val < x) //iterate through all nodes that do not need rearranging
 	{
 		insertAfter = ptr;
 		prev = ptr;
 		ptr = ptr->next;
 	}
     
-    while(ptr != NULL){
-        if(ptr -> val < x){
+    while(ptr != NULL){ //first node that is out of place has been hit now
+        if(ptr -> val < x){ //place before the partition
             prev -> next = ptr -> next;
             ptr -> next = insertAfter -> next;
             insertAfter -> next = ptr;
                         
             ptr = prev -> next;
             insertAfter = insertAfter -> next;
-        }else{
+        }else{ //node belongs in current position, move on to next
             prev = ptr;
             ptr = ptr -> next;
         }
     }
     
-    return dummyHead -> next;
+    ptr = dummyHead -> next;
+    free(dummyHead); //prevent mem leak
+    return ptr;
 }
